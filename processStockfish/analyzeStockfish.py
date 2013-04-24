5ed905a4fab155ebaa3e70bd90884c9553d804d3
Diego Fernandez Slezak <dfslezak@gmail.com>
2013-04-24 08:44:30 -0300

    Fixes #50 - Converted files to PEP8 standard using autopep8
    autopep8 was installed using pip, instead of apt as it was not in the repositories.

--- processStockfish/analyzeStockfish.py
+++ processStockfish/analyzeStockfish.py
@@ -5,197 +5,213 @@ import pgn
 from subprocess import *
 import select
 import time
-import sys, getopt
+import sys
+import getopt
+
 
 def cleanmove(move):
-	if move[-1]=='+':
-		move = move[0:-1]
-	return move
-	
-def analyzeMove(stockfish,move_type,white_turn,depth,fromdepth):
-	#score = None
-	score_array = [None]*(depth-fromdepth+1)
-	
-	# First, check that move is not checkmate
-	if move_type[-1]=="#":
-		for i in range(fromdepth,depth+1):
-			score_array[i-fromdepth] = -99999
-	else:
-		# Send move_type to stockfish
-		cmd = "position moves " + str(move_type) + "\n"
-		stockfish.stdin.write(cmd)
-		#print cmd
-
-		# Start analysis
-		cmd = "go depth " + str(depth) + "\n"
-		stockfish.stdin.write(cmd)
-		#print cmd
-
-		# Search score in analysis
-		search_string = "info depth " #+str(depth)+" score "
-		end_string = "bestmove"
-		line = ''
-		while not end_string in line:
-			line = stockfish.stdout.readline()
-			#print str(line)
-			
-			# If line includes "lowerbound" or "upperbound", skip it.
-			# If line includes "mate", then score is 999 or -999
-			if search_string in line and "score" in line and not "mate" in line and not "lowerbound" in line and not "upperbound" in line:
-				line = line.split()
-				line_depth = int(line[2])
-				if line_depth>=fromdepth:
-					line_score = line[line.index("score")+2]
-					#print line
-					#print line_score
-					#print "line_depth="+str(line_depth)
-					#print "fromdepth="+str(fromdepth)
-					score_array[line_depth-fromdepth] = int(line_score)
-					#print str(line)
-
-			if search_string in line and "mate" in line:
-				line = line.split()
-				line_depth = int(line[2])
-				if line_depth>=fromdepth:
-					if (int(line[line.index("mate")+1])<0): sign = -1
-					else: sign = 1
-					for i in range(line_depth,depth+1):
-						score_array[i-fromdepth] = 99999*sign
-					#line = end_string
-					#print "Detected mate: " + str(score_array)
-				else:
-					if (int(line[line.index("mate")+1])<0): sign = -1
-					else: sign = 1
-					for i in range(fromdepth,depth+1):
-						score_array[i-fromdepth] = 99999*sign
-					#print str(line)
-					#line = end_string
+    if move[-1] == '+':
+        move = move[0:-1]
+    return move
 
 
-			if "Segmentation" in line:
-				print line
-				exit(0)
-			#print str(line)
-	
-	if not white_turn:
-		for i in range(fromdepth,depth+1):
-			if score_array[i-fromdepth]!=None:
-				score_array[i-fromdepth] = -int(score_array[i-fromdepth])
+def analyzeMove(stockfish, move_type, white_turn, depth, fromdepth):
+    # score = None
+    score_array = [None]*(depth-fromdepth+1)
 
-	#print '--- Score: ' + str(score_array)
-	return score_array
+    # First, check that move is not checkmate
+    if move_type[-1] == "#":
+        for i in range(fromdepth, depth+1):
+            score_array[i-fromdepth] = -99999
+    else:
+        # Send move_type to stockfish
+        cmd = "position moves " + str(move_type) + "\n"
+        stockfish.stdin.write(cmd)
+        # print cmd
 
-def analyzeGame(pgntext,depth,fromdepth):
-	start_ag = time.time()
+        # Start analysis
+        cmd = "go depth " + str(depth) + "\n"
+        stockfish.stdin.write(cmd)
+        # print cmd
 
-	# *********** Convert SAN format to Long Algebraic ************
-	pgn_extract = Popen(["pgn-extract", "-Wlalg","-C","-N","--notags","--noresults","--nomovenumbers"],bufsize=0,stdin=PIPE, stdout=PIPE,stderr=STDOUT, shell=False)
-	pgn_extract.stdin.write(pgntext)
-	
-	pgn_splitted = pgntext.split()
-	#end_code = str(pgn_splitted[pgn_splitted.index('[EndCode')+1].strip('"]'))
-	
-	moves_alg = pgn_extract.communicate()[0].split("\n")
-	moves = " ".join(moves_alg[3:-1]).split()
-	#print " ".join(moves)
+        # Search score in analysis
+        search_string = "info depth "  # +str(depth)+" score "
+        end_string = "bestmove"
+        line = ''
+        while not end_string in line:
+            line = stockfish.stdout.readline()
+            # print str(line)
 
-	end_pgnExtract = time.time()
+            # If line includes "lowerbound" or "upperbound", skip it.
+            # If line includes "mate", then score is 999 or -999
+            if search_string in line and "score" in line and not "mate" in line and not "lowerbound" in line and not "upperbound" in line:
+                line = line.split()
+                line_depth = int(line[2])
+                if line_depth >= fromdepth:
+                    line_score = line[line.index("score")+2]
+                    # print line
+                    # print line_score
+                    # print "line_depth="+str(line_depth)
+                    # print "fromdepth="+str(fromdepth)
+                    score_array[line_depth-fromdepth] = int(line_score)
+                    # print str(line)
 
-	## ************ Start Stockfish ************
-	stockfish = Popen("./stockfish.out",bufsize=0,stdin=PIPE, stdout=PIPE,stderr=STDOUT, universal_newlines=True,shell=False)
-	# Set uci interface
-	cmd = "uci\n"
-	stockfish.stdin.write(cmd)
-	#print "SENDING COMMAND:  " + str(cmd)
+            if search_string in line and "mate" in line:
+                line = line.split()
+                line_depth = int(line[2])
+                if line_depth >= fromdepth:
+                    if (int(line[line.index("mate")+1]) < 0):
+                        sign = -1
+                    else:
+                        sign = 1
+                    for i in range(line_depth, depth+1):
+                        score_array[i-fromdepth] = 99999*sign
+                    # line = end_string
+                    # print "Detected mate: " + str(score_array)
+                else:
+                    if (int(line[line.index("mate")+1]) < 0):
+                        sign = -1
+                    else:
+                        sign = 1
+                    for i in range(fromdepth, depth+1):
+                        score_array[i-fromdepth] = 99999*sign
+                    # print str(line)
+                    # line = end_string
 
-	# Check for initialization
-	line=''
-	while not "uciok" in line:
-		line = stockfish.stdout.readline()
-		
-	# Set analyse mode in stockfish
-	cmd = "setoption name UCI_AnalyseMode value true\n"
-	stockfish.stdin.write(cmd)
-	#print "SENDING COMMAND:  " + str(cmd)
+            if "Segmentation" in line:
+                print line
+                exit(0)
+            # print str(line)
 
-	# Start new game
-	cmd = "ucinewgame\n"
-	stockfish.stdin.write(cmd)
-	#print "SENDING COMMAND:  " + str(cmd)
+    if not white_turn:
+        for i in range(fromdepth, depth+1):
+            if score_array[i-fromdepth] != None:
+                score_array[i-fromdepth] = -int(score_array[i-fromdepth])
 
-	end_fork = time.time()
-
-	## *********** Analyse game ***********
-	scores =[]
-	score_array = [None]*(depth-fromdepth+1)
-	# Start with black turns (white has already moved the first ply.
-	white_turn = False
-	# For each move, run stockfish analyzer...
-	for i in range(len(moves)):
-		move = moves[i]
-		#print 'Analysing move ' + str(move)
-		
-		# If this is the last move, check the end_code. If it is "Sta" then score = 0
-		if (i==len(moves)-1) and (('drawn' in pgntext) or ('Neither player' in pgntext)): 
-			for i in range(fromdepth,depth+1):
-				score_array[i-fromdepth] = 0
-		else: 
-			score_array = analyzeMove(stockfish,move,white_turn,depth,fromdepth)
-		
-		scores = scores + [[str(move), score_array]]
-		white_turn = not white_turn
-		
-	# Quit stockfish
-	cmd = "quit\n"
-	stockfish.stdin.write(cmd)
-	stockfish.communicate()
-
-	end_analysis = time.time()
-	#print 'Time control: PGN_EXTRACT='+str(end_pgnExtract-start_ag)+' --- STOCKFISH_FORK='+str(end_fork-end_pgnExtract)+' --- ANALYSIS='+str(end_analysis-end_fork) 
+    # print '--- Score: ' + str(score_array)
+    return score_array
 
 
-	return scores
-	
+def analyzeGame(pgntext, depth, fromdepth):
+    start_ag = time.time()
+
+    # *********** Convert SAN format to Long Algebraic ************
+    pgn_extract = Popen(
+        ["pgn-extract", "-Wlalg", "-C", "-N", "--notags", "--noresults",
+                        "--nomovenumbers"], bufsize=0, stdin=PIPE, stdout=PIPE, stderr=STDOUT, shell=False)
+    pgn_extract.stdin.write(pgntext)
+
+    pgn_splitted = pgntext.split()
+    # end_code =
+    # str(pgn_splitted[pgn_splitted.index('[EndCode')+1].strip('"]'))
+
+    moves_alg = pgn_extract.communicate()[0].split("\n")
+    moves = " ".join(moves_alg[3:-1]).split()
+    # print " ".join(moves)
+
+    end_pgnExtract = time.time()
+
+    ## ************ Start Stockfish ************
+    stockfish = Popen("./stockfish.out", bufsize=0, stdin=PIPE,
+                      stdout=PIPE, stderr=STDOUT, universal_newlines=True, shell=False)
+    # Set uci interface
+    cmd = "uci\n"
+    stockfish.stdin.write(cmd)
+    # print "SENDING COMMAND:  " + str(cmd)
+
+    # Check for initialization
+    line = ''
+    while not "uciok" in line:
+        line = stockfish.stdout.readline()
+
+    # Set analyse mode in stockfish
+    cmd = "setoption name UCI_AnalyseMode value true\n"
+    stockfish.stdin.write(cmd)
+    # print "SENDING COMMAND:  " + str(cmd)
+
+    # Start new game
+    cmd = "ucinewgame\n"
+    stockfish.stdin.write(cmd)
+    # print "SENDING COMMAND:  " + str(cmd)
+
+    end_fork = time.time()
+
+    ## *********** Analyse game ***********
+    scores = []
+    score_array = [None]*(depth-fromdepth+1)
+    # Start with black turns (white has already moved the first ply.
+    white_turn = False
+    # For each move, run stockfish analyzer...
+    for i in range(len(moves)):
+        move = moves[i]
+        # print 'Analysing move ' + str(move)
+
+        # If this is the last move, check the end_code. If it is "Sta" then
+        # score = 0
+        if (i == len(moves)-1) and (('drawn' in pgntext) or ('Neither player' in pgntext)):
+            for i in range(fromdepth, depth+1):
+                score_array[i-fromdepth] = 0
+        else:
+            score_array = analyzeMove(
+                stockfish, move, white_turn, depth, fromdepth)
+
+        scores = scores + [[str(move), score_array]]
+        white_turn = not white_turn
+
+    # Quit stockfish
+    cmd = "quit\n"
+    stockfish.stdin.write(cmd)
+    stockfish.communicate()
+
+    end_analysis = time.time()
+    # print 'Time control: PGN_EXTRACT='+str(end_pgnExtract-start_ag)+' ---
+    # STOCKFISH_FORK='+str(end_fork-end_pgnExtract)+' --- ANALYSIS='+str
+    # (end_analysis-end_fork)
+
+    return scores
+
 if __name__ == "__main__":
-	fileName = "prueba2.pgn"
-	depth = 8
+    fileName = "prueba2.pgn"
+    depth = 8
 
-	# Check command-line arguments. If no argument is provided, just run the test file.
-	try:
-		opts, args = getopt.getopt(sys.argv[1:], "d:f:m:", ["depth","file","fromdepth"])
-	except getopt.GetoptError, err:
-		# print help information and exit:
-		print str(err) # will print something like "option -a not recognized"
-		usage()
-		sys.exit(2)
-	
-	for o, a in opts:
-		if o in ("-d", "--depth"):
-			depth = int(a)
-		elif o in ("-f", "--file"):
-			fileName = a
-		elif o in ("-m", "--fromdepth"):
-			fromdepth = int(a)
-		else:
-			assert False, "unhandled option"
-	#output = None
-	#if len(sys.argv)>1:
-		#fileName = sys.argv[1]
-	#else:
-		
-	# Open PGN file
-	f = open(fileName,'r')
-	pgntext = f.read()
-	f.close()
-	
-	# Send it to analyzer.
-	scores = analyzeGame(pgntext,depth,fromdepth)
-	
-	old_score = [None]*(depth-fromdepth+1)
-	for i in range(fromdepth,depth+1):
-		old_score[i-fromdepth] = 0
-	white_turn = 1
-	for move,score in scores:
-		print "Move: " + str(move) + " --- score is " + str(score) + ' --- delta_score=' + str((int(score[5]) - int(old_score[6]))*white_turn)
-		old_score = score
-		white_turn = -white_turn
+    # Check command-line arguments. If no argument is provided, just run the
+    # test file.
+    try:
+        opts, args = getopt.getopt(sys.argv[
+                                   1:], "d:f:m:", ["depth", "file", "fromdepth"])
+    except getopt.GetoptError, err:
+        # print help information and exit:
+        print str(err)  # will print something like "option -a not recognized"
+        usage()
+        sys.exit(2)
+
+    for o, a in opts:
+        if o in ("-d", "--depth"):
+            depth = int(a)
+        elif o in ("-f", "--file"):
+            fileName = a
+        elif o in ("-m", "--fromdepth"):
+            fromdepth = int(a)
+        else:
+            assert False, "unhandled option"
+    # output = None
+    # if len(sys.argv)>1:
+        # fileName = sys.argv[1]
+    # else:
+
+    # Open PGN file
+    f = open(fileName, 'r')
+    pgntext = f.read()
+    f.close()
+
+    # Send it to analyzer.
+    scores = analyzeGame(pgntext, depth, fromdepth)
+
+    old_score = [None]*(depth-fromdepth+1)
+    for i in range(fromdepth, depth+1):
+        old_score[i-fromdepth] = 0
+    white_turn = 1
+    for move, score in scores:
+        print "Move: " + str(move) + " --- score is " + str(score) + ' --- delta_score=' + str((int(score[5]) - int(old_score[6]))*white_turn)
+        old_score = score
+        white_turn = -white_turn
--- processStockfish/runStockfishAnalysis.py
+++ processStockfish/runStockfishAnalysis.py
@@ -5,7 +5,8 @@ import pgn
 from subprocess import *
 import select
 import time
-import sys, getopt
+import sys
+import getopt
 import analyzeStockfish
 import psycopg2
 import random
@@ -18,136 +19,151 @@ print '---- Starting analysis on ' + str(time.ctime()) + '----'
 depth = 8
 numgames = 1
 
-# Check command-line arguments. If no argument is provided, just run the test file.
+# Check command-line arguments. If no argument is provided, just run the
+# test file.
 try:
-	opts, args = getopt.getopt(sys.argv[1:], "d:n:m:", ["depth","numgames","fromdepth"])
+    opts, args = getopt.getopt(sys.argv[
+                               1:], "d:n:m:", ["depth", "numgames", "fromdepth"])
 except getopt.GetoptError, err:
-	print str(err) # will print something like "option -a not recognized"
-	sys.exit(2)
+    print str(err)  # will print something like "option -a not recognized"
+    sys.exit(2)
 
 for o, a in opts:
-	if o in ("-d", "--depth"):
-		depth = int(float(a))
-	elif o in ("-n", "--numgames"):
-		numgames = int(float(a))
-	elif o in ("-m", "--fromdepth"):
-		fromdepth = int(float(a))
-	else:
-		assert False, "unhandled option"
+    if o in ("-d", "--depth"):
+        depth = int(float(a))
+    elif o in ("-n", "--numgames"):
+        numgames = int(float(a))
+    elif o in ("-m", "--fromdepth"):
+        fromdepth = int(float(a))
+    else:
+        assert False, "unhandled option"
 
 # Query prefix for adding moves...
-sql_insert = 'INSERT INTO move_properties (move' #stockfish_score, stockfish_delta_score) VALUES '
-for i in range(fromdepth,depth+1):
-	sql_insert += ', score_depth' + str(i)
+sql_insert = 'INSERT INTO move_properties (move'  # stockfish_score, stockfish_delta_score) VALUES '
+for i in range(fromdepth, depth+1):
+    sql_insert += ', score_depth' + str(i)
 sql_insert += ', delta_score) VALUES '
-#print sql_insert
+# print sql_insert
 
 # Connect to the database
 try:
-	conn = psycopg2.connect("dbname='ficsdb' user='dfslezak' host='calamaro.exp.dc.uba.ar' password='23571113'")
-	cursor = conn.cursor()
+    conn = psycopg2.connect(
+        "dbname='ficsdb' user='dfslezak' host='calamaro.exp.dc.uba.ar' password='23571113'")
+    cursor = conn.cursor()
 except:
-	text = "I am unable to connect to the database"
-	print text
-	exit(0)
+    text = "I am unable to connect to the database"
+    print text
+    exit(0)
 
 for i in range(numgames):
-	# Get an unanalysed game from database
-	#total_time = random.choice([1,1,1,2,3,3,3,4,5,5,5,6,7,8,9,10,10,10,11,12,13,14])
-	#increment = 0
-	nGames = 1
+    # Get an unanalysed game from database
+    # total_time = random.choice([1,1,1,2,3,3,3,4,5,5,5,6,7,8,9,10,10,10,11,12,13,14])
+    # increment = 0
+    nGames = 1
 
-	# Get the min and max id_game to throw a random number...
-	#statement = "SELECT min(id_game),max(id_game) FROM games WHERE games.increment="+str(increment)+" AND games.total_time="+str(total_time)
-	statement = "SELECT min(id_game),max(id_game) FROM games"
-	cursor.execute(statement)
-	result = cursor.fetchone()
-	min_id_game = result[0]
-	max_id_game = result[1]
-	random_game = random.randint(min_id_game, max_id_game)
-	print 'Selecting game from ' + str(min_id_game) + ' to ' +str(max_id_game)+': '+str(random_game)
+    # Get the min and max id_game to throw a random number...
+    # statement = "SELECT min(id_game),max(id_game) FROM games WHERE
+    # games.increment="+str(increment)+" AND
+    # games.total_time="+str(total_time)
+    statement = "SELECT min(id_game),max(id_game) FROM games"
+    cursor.execute(statement)
+    result = cursor.fetchone()
+    min_id_game = result[0]
+    max_id_game = result[1]
+    random_game = random.randint(min_id_game, max_id_game)
+    print 'Selecting game from ' + str(min_id_game) + ' to ' + str(max_id_game)+': '+str(random_game)
 
-	# Get unanalysed game.
-	#statement = "SELECT id_game, get_game(id_game) FROM games WHERE id_game>"+str(random_game)+" AND (stockfish_analysed IS NULL OR NOT stockfish_analysed) AND ply_count>5 AND ply_count<200 AND game_type<>4 AND game_type<>5 ORDER BY id_game LIMIT 1;"
-	statement = "SELECT id_game, get_game(id_game) FROM games JOIN users as u1 ON u1.id_user=games.w_user JOIN users as u2 ON u2.id_user=games.b_user WHERE id_game>"+str(random_game)+" AND (stockfish_analysed IS NULL OR NOT stockfish_analysed) AND ply_count>5 AND ply_count<200 AND game_type<>4 AND game_type<>5 AND (u1.timezone is NOT NULL OR u2.timezone is NOT NULL) AND (u1.has_chronotype OR u2.has_chronotype) ORDER BY id_game LIMIT 1;"
-        	cursor.execute(statement)
-	result = cursor.fetchone()
-	# If no more games to analyse, simpley start all over again choosing another total_time.
-	if len(result)==0: 
-		#print 'No more games of total_time='+str(total_time)
-		print 'No more games to analyse'
-		continue
-	
-	id_game = result[0]
-	print 'Analysing game '+str(id_game)
-	pgntext = result[1]
-	pgntext_split = result[1].split()
-	plyCount = pgntext_split[pgntext_split.index('[PlyCount')+1].strip('"[]')
-	
-	# Get unanalysed game.
-	statement = "SELECT id_move, move_number,w_b FROM moves WHERE game=" + str(id_game) + "ORDER BY move_number,w_b"
-	cursor.execute(statement)
-	result = cursor.fetchall()
-	ID_MOVE=0
+    # Get unanalysed game.
+    # statement = "SELECT id_game, get_game(id_game) FROM games WHERE
+    # id_game>"+str(random_game)+" AND (stockfish_analysed IS NULL OR NOT
+    # stockfish_analysed) AND ply_count>5 AND ply_count<200 AND game_type<>4
+    # AND game_type<>5 ORDER BY id_game LIMIT 1;"
+    statement = "SELECT id_game, get_game(id_game) FROM games JOIN users as u1 ON u1.id_user=games.w_user JOIN users as u2 ON u2.id_user=games.b_user WHERE id_game>"+str(
+        random_game)+" AND (stockfish_analysed IS NULL OR NOT stockfish_analysed) AND ply_count>5 AND ply_count<200 AND game_type<>4 AND game_type<>5 AND (u1.timezone is NOT NULL OR u2.timezone is NOT NULL) AND (u1.has_chronotype OR u2.has_chronotype) ORDER BY id_game LIMIT 1;"
+    cursor.execute(statement)
+    result = cursor.fetchone()
+    # If no more games to analyse, simpley start all over again choosing
+    # another total_time.
+    if len(result) == 0:
+        # print 'No more games of total_time='+str(total_time)
+        print 'No more games to analyse'
+        continue
 
-	# Now that we have the PGN text, send it to stockfish to analyse it...
-	scores = analyzeStockfish.analyzeGame(pgntext,depth,fromdepth)
+    id_game = result[0]
+    print 'Analysing game '+str(id_game)
+    pgntext = result[1]
+    pgntext_split = result[1].split()
+    plyCount = pgntext_split[pgntext_split.index('[PlyCount')+1].strip('"[]')
 
-	# Check that stockfish analysed all moves and generate SQL query for insertion
-	if int(len(scores)) == int(plyCount):
-		sql_insert_all = sql_insert
-		iterator = 0
-		for move,score_array in scores:
-			# Initialize move-values string and calculate delta_score
-			if iterator>0: 
-				if (iterator % 2==0): sign = 1.0
-				else: sign = (-1.0)
-				delta_score = sign*(float(score_array[5])-float(scores[iterator-1][1][6]))
-				sql_move=','
-			else: 
-				delta_score = score_array[6]
-				sql_move=''
-				
-			
-			# Create tuple for move-values string
-			sql_move+='(' + str(result[iterator][ID_MOVE]) + ','
-			for i in range(fromdepth,depth+1):
-				sql_move+= str(float(score_array[i-fromdepth])/100.0) + ','
-			sql_move += str(float(delta_score)/100.0)+')'
-			
-			# Append to SQL query.
-			sql_insert_all += sql_move
-			
-			# Increment iterator
-			iterator += 1
-			
-			#print "Move: " + str(move) + " --- score is " + str(score)
-		sql_insert_all += ";"
-		cursor.execute(sql_insert_all)
-		conn.commit()
-		#print sql_insert_all
+    # Get unanalysed game.
+    statement = "SELECT id_move, move_number,w_b FROM moves WHERE game=" + \
+        str(id_game) + "ORDER BY move_number,w_b"
+    cursor.execute(statement)
+    result = cursor.fetchall()
+    ID_MOVE = 0
 
-	else:
-		print "Game " + str(id_game) + ': Something went wrong. Different amount of moves and scores.'
-		exit(0)
-		
-	# After inserting moves, we have to check "stockfish_analyzed".
-	statement = "SET enable_seqscan=false; SELECT check_stockfish_analysed_game("+ str(id_game) + ");";
-	cursor.execute(statement)
-	result = cursor.fetchone()
+    # Now that we have the PGN text, send it to stockfish to analyse it...
+    scores = analyzeStockfish.analyzeGame(pgntext, depth, fromdepth)
 
-	if result[0] == True:
-		text = "Game " + str(id_game) + ": All scores inserted!!"
-		print text
-	else:
-		text = "Game " + str(id_game) + ": Some score is missing..."
-		print text
+    # Check that stockfish analysed all moves and generate SQL query for
+    # insertion
+    if int(len(scores)) == int(plyCount):
+        sql_insert_all = sql_insert
+        iterator = 0
+        for move, score_array in scores:
+            # Initialize move-values string and calculate delta_score
+            if iterator > 0:
+                if (iterator % 2 == 0):
+                    sign = 1.0
+                else:
+                    sign = (-1.0)
+                delta_score = sign*(float(score_array[
+                                    5])-float(scores[iterator-1][1][6]))
+                sql_move = ','
+            else:
+                delta_score = score_array[6]
+                sql_move = ''
 
-	conn.commit()
+            # Create tuple for move-values string
+            sql_move += '(' + str(result[iterator][ID_MOVE]) + ','
+            for i in range(fromdepth, depth+1):
+                sql_move += str(float(score_array[i-fromdepth])/100.0) + ','
+            sql_move += str(float(delta_score)/100.0)+')'
+
+            # Append to SQL query.
+            sql_insert_all += sql_move
+
+            # Increment iterator
+            iterator += 1
+
+            # print "Move: " + str(move) + " --- score is " + str(score)
+        sql_insert_all += ";"
+        cursor.execute(sql_insert_all)
+        conn.commit()
+        # print sql_insert_all
+
+    else:
+        print "Game " + str(id_game) + ': Something went wrong. Different amount of moves and scores.'
+        exit(0)
+
+    # After inserting moves, we have to check "stockfish_analyzed".
+    statement = "SET enable_seqscan=false; SELECT check_stockfish_analysed_game(" + str(
+        id_game) + ");"
+    cursor.execute(statement)
+    result = cursor.fetchone()
+
+    if result[0] == True:
+        text = "Game " + str(id_game) + ": All scores inserted!!"
+        print text
+    else:
+        text = "Game " + str(id_game) + ": Some score is missing..."
+        print text
+
+    conn.commit()
 
 # Close database connection...
 conn.close()
 
 # End time...
 end_time = time.time()
-print '---- Ending analysis on ' + str(time.ctime()) + ' (Elapsed time: ' + str(end_time-start_time)+ 'seconds) ----'
+print '---- Ending analysis on ' + str(time.ctime()) + ' (Elapsed time: ' + str(end_time-start_time) + 'seconds) ----'
